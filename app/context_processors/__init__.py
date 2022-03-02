7db8e694742a8f368bd634c3d74f6a53b898baa0
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-03-01 20:10:11 -0500

    Updated css

--- app/context_processors/__init__.py
+++ app/context_processors/__init__.py
@@ -2,7 +2,7 @@ from os import getenv
 import datetime
 
 def utility_text_processors():
-    message = "hello world"
+    message = "Thank You For Visiting"
 
     def deployment_environment():
         return getenv('FLASK_ENV', None)
