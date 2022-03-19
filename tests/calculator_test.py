35ef7726b76ac634626e8f452e153b0b0129f509
Diana Zawislak <diana.zawislak@icloud.com>
2022-03-19 18:26:01 -0400

    fixed majority of problems in calculator

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -30,24 +30,26 @@ def test_calculator_multiply_method():
     tuple_list = (2, 4)
     assert Calculator.multiply(tuple_list) == 8
 
-    def test_calculator_divide_method():
-        """ Testing the calculator division"""
-        # Arrange
-        tuple_list = (2, 4)
-        # Act
-        result = Calculator.divide(tuple_list)
-        # Assert
-        assert result == 0.5
 
-    def test_calculator_division_exception():
-        """ Testing division exception for division by zero"""
-        # Arrange
-        value_a = 1
-        value_b = 0
-        # Act
-        result = Calculator
+def test_calculator_divide_method():
+    """ Testing the calculator division"""
+    # Arrange
+    tuple_list = (2, 4)
+    # Act
+    result = Calculator.divide(tuple_list)
+    # Assert
+    assert result == 0.5
 
-        with pytest.raises(ZeroDivisionError):
-            Calculator.divide_numbers(value_a, value_b)
-        # ASSERT
-        assert result is True
+
+def test_calculator_division_exception():
+    """ Testing division exception for division by zero"""
+    # Arrange
+    value_a = 2
+    value_b = 0
+    # Act
+    result = Calculator
+
+    with pytest.raises(ZeroDivisionError):
+        Calculator.divide_numbers(value_a, value_b)
+    # ASSERT
+    assert result is True
