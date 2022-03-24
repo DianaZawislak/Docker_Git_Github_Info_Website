45f0a3434a05395fb0a6b94c6c03078dad48ca6c
Diana Zawislak <diana.zawislak@icloud.com>
2022-03-23 21:38:55 -0400

    Added new tests to simple pages test testing all new pages

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,5 +1,6 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This tests menu links"""
     response = client.get("/")
@@ -10,11 +11,13 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page3">Python and Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
 
+
 def test_request_index(client):
     """This tests home page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Home" in response.data
+
 
 def test_request_about(client):
     """This tests about page"""
@@ -22,11 +25,13 @@ def test_request_about(client):
     assert response.status_code == 200
     assert b"About" in response.data
 
+
 def test_request_page1(client):
     """This tests page1 page"""
     response = client.get("/page1")
     assert response.status_code == 200
     assert b"Git and Github" in response.data
+
 
 def test_request_page2(client):
     """This tests page2 page"""
@@ -34,17 +39,47 @@ def test_request_page2(client):
     assert response.status_code == 200
     assert b"Docker" in response.data
 
+
 def test_request_page3(client):
     """This tests page3 page"""
     response = client.get("/page3")
     assert response.status_code == 200
     assert b"Python and Flask" in response.data
 
+
 def test_request_page4(client):
     """This tests page4 page"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"CI/CD" in response.data
+
+
+def test_request_AAAtesting(client):
+    """This tests AAA page"""
+    response = client.get("/AAAtesting")
+    assert response.status_code == 200
+    assert b"What is AAA testing?" in response.data
+
+
+def test_request_OOP(client):
+    """This tests OOP page"""
+    response = client.get("/OOP")
+    assert response.status_code == 200
+    assert b"Local variables altered in the process are separate from local variables" in response.data
+
+
+def test_request_pyliny(client):
+    """This tests Pylint page"""
+    response = client.get("/pyliny")
+    assert response.status_code == 200
+    assert b"What is Pytest?" in response.data
+
+
+def test_request_solid(client):
+    """This tests SOLID page"""
+    response = client.get("/solid")
+    assert response.status_code == 200
+    assert b"SOLIDbanner.png" in response.data
 
 def test_request_page_not_found(client):
     """This tests page not found page"""
